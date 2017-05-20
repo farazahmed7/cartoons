10cabd269fef7956db5218614e4cc5fdbde56a3a
faraz <faraz.dipsite@gmail.com>
2017-05-20 07:39:08 +0530

    fixed

--- HindiCartoon/admin.py
+++ HindiCartoon/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from HindiCartoon.models import Cartoon,Episode, Season
+from HindiCartoon.models import Cartoon,Episode, Season,RequestCartoon
 
 admin.site.register(Cartoon)
 admin.site.register(Episode)
 admin.site.register(Season)
+admin.site.register(RequestCartoon)
